c5a49d278d109d92bccd8e244723929b9dcba684
lakshaybomotra <lakshaybomotra@gmail.com>
2024-12-12 16:04:23 +0530

    BottomTab

---
+++ src/BottomTabs.js
@@ -0,0 +1,58 @@
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import Home from "./Screens/Home/Home";
+// import Transactions from "./Screens/Transactions/Transactions";
+// import Profile from "./Screens/Profile/Profile";
+
+// const Tab = createBottomTabNavigator();
+
+// export default function BottomTabs() {
+//     return (
+//         <Tab.Navigator>
+//             <Tab.Screen name="Home" component={Home}/>
+//             <Tab.Screen name="Transactions" component={Transactions}/>
+//             <Tab.Screen name="Profile" component={Profile}/>
+//         </Tab.Navigator>
+//     )
+// }
+
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./Screens/Home/Home";
+import Transactions from "./Screens/Transactions/Transactions";
+import Profile from "./Screens/Profile/Profile";
+import Icon from 'react-native-vector-icons/FontAwesome';
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabs() {
+    return (
+        <Tab.Navigator
+
+            screenOptions={{
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'green',
+            }}
+        >
+            <Tab.Screen name='Home' component={Home}
+                options={{
+                    tabBarIcon: () => (
+                        <Icon name='home' size={20} />
+                    )
+                }}
+            />
+            <Tab.Screen name='Transactions' component={Transactions}
+                options={{
+                    tabBarIcon: () => (
+                        <Icon name='gear' size={20} />
+                    )
+                }}
+            />
+            <Tab.Screen name='Profile' component={Profile}
+                options={{
+                    tabBarIcon: () => (
+                        <Icon name='user' size={20} />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
---
+++ src/Screens/Transactions/Transactions.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from 'react-native';
+function Transactions() {
+    return (
+        <View>
+            <Text>
+                This is the Transactions page
+            </Text>
+        </View>
+    )
+
+}
+
+export default Transactions;
