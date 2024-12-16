c0b06567291f1643febb41db71710d596f9f9141
Lakshay Bomotra <lakshaybomotra@gmail.com>
2024-12-16 17:32:15 +0530

    Fcm working

--- src/Screens/Profile/Profile.js
+++ src/Screens/Profile/Profile.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 function Profile() {
+    const getDeviceToken = async () => {
+        const token = await messaging().getToken();
+        console.log(token);
+    };
+    useEffect(() => {
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+          Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+
+        getDeviceToken();
+
+        return unsubscribe;
+      }, []);
+
     return (
         <View>
             <Text>
                 This is the profile page
             </Text>
         </View>
-    )
+    );
 
 }
 
